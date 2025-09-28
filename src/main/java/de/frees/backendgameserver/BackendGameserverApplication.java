a634e009adbffe22445358083181340924303299
shinydesteny1512 <rfsmf2@gmail.com>
2025-09-28 20:48:37 +0200

    Initial Commit

---
+++ src/main/java/de/frees/backendgameserver/BackendGameserverApplication.java
@@ -0,0 +1,13 @@
+package de.frees.backendgameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGameserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendGameserverApplication.class, args);
+    }
+
+}
