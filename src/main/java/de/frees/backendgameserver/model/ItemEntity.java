12462fb7a6fa0392e848337d314fb856a15806c3
shinydesteny1512 <rfsmf2@gmail.com>
2025-10-01 23:19:08 +0200

    Adjusted Application and Entity to new Mongo Impl

--- src/main/java/de/frees/backendgameserver/model/ItemEntity.java
+++ src/main/java/de/frees/backendgameserver/model/ItemEntity.java
@@ -4,20 +4,22 @@ import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+@Document(collection = "items")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class ItemEntity {
 
-  @MongoId private UUID itemId;
+    @MongoId private UUID itemId;
 
-  private String itemName;
+    private String itemName;
 
-  private String description;
+    private String description;
 
-  private Double price;
+    private Double price;
 
-  private String category;
+    private String category;
 }
