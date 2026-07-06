0103eb0d43fbf4bb1e47e1fbc1f593b8d622171f
shinydesteny1512 <rfsmf2@gmail.com>
2026-07-06 19:34:17 +0200

    Adds_Item_APIs: Solved Merge Conflicts and Adds Given/When/Then to tests

--- src/test/java/de/frees/backendgameserver/service/ItemServiceTest.java
+++ src/test/java/de/frees/backendgameserver/service/ItemServiceTest.java
@@ -1,19 +1,19 @@
 package de.frees.backendgameserver.service;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import de.frees.backendgameserver.repository.ItemRepository;
 import de.frees.backendgameserver.exception.objects.ItemNotFoundException;
 import de.frees.backendgameserver.mapper.ItemMapper;
+import de.frees.backendgameserver.repository.ItemRepository;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(MockitoExtension.class)
 class ItemServiceTest {
@@ -26,19 +26,26 @@ class ItemServiceTest {
 
   @Test
   void deleteById_notFound_throws() {
+    // given
     UUID id = UUID.randomUUID();
+
+    // when
     when(itemRepository.existsByItemId(id.toString())).thenReturn(false);
 
+    // then
     assertThatThrownBy(() -> itemService.deleteById(id)).isInstanceOf(ItemNotFoundException.class);
   }
 
   @Test
   void deleteById_existing_deletesAndReturnsDeletedId() {
+    // given
     UUID id = UUID.randomUUID();
     when(itemRepository.existsByItemId(id.toString())).thenReturn(true);
 
+    // when
     UUID deletedId = itemService.deleteById(id);
 
+    // then
     assertThat(deletedId).isEqualTo(id);
     verify(itemRepository).deleteByItemId(id.toString());
   }
